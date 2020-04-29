1dcce812f561a0ab80e29763918c8963fb156179
Erica Akira <Erica@MacBook-Air-de-Erica.local>
2020-04-29 15:30:31 -0300

    cirado model Post

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+let Post = (sequelize, DataTypes) => {
+    return sequelize.define(
+        'Post',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull:false
+            },
+            texto: {
+                type: DataTypes.STRING(100),
+                allowNull: false
+            },
+            img: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            usuarios_id: {
+                type: DataTypes.INTEGER,
+                foreignKey: true,
+                allowNull: false
+            },
+            n_likes: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        },{
+            tableName: "posts",
+            timestamps: false
+        }
+
+    );
+}
+
+module.exports = Post;
